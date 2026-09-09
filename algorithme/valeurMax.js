81b643a7670800e5e96305e128a78587c293d32f
Tasnime <tasnimsahli97@gmail.com>
2026-09-09 10:31:07 +0100

    valeur maximale
    Removed the implementation of the maximum value finder.

--- algorithme/valeurMax.js
+++ algorithme/valeurMax.js
@@ -1,11 +1,1 @@
-//valeur maximale 
-let array=[50,2,100,350,5];
 
-let vMax=array[0];
-
-for(i=0;i<array.length ;i++){
-    if(array[i]>vMax){
-        vMax=array[i];
-    }
-}
-console.log("La valeur maximale est : " + vMax);
