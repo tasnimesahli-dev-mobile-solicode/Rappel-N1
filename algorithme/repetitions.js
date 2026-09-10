bf1906a60db7a6dca581ce9b3d8a70be363d1326
tasnimsahli97@gmail.com <tasnimsahli97@gmail.com>
2026-09-10 10:56:45 +0100

    "change something"

--- algorithme/repetitions.js
+++ algorithme/repetitions.js
@@ -5,19 +5,14 @@ for(let i=0 ; i<nombres.length ; i++){
     for(let j=0 ; j<nombres.length ; j++){
         if(nombres[i]==nombres[j] && i!==j){
         computer++;
-        }
-
-    }
+        }}
     if(computer>=1){
         let existe=false;
        for(let o=0 ; o<tab.length ; o++){
         if(tab[o]===nombres[i]){
             existe=true;
-        }
-       }
+        }}
        if(existe==false){
         tab.push(nombres[i]);
-       }
-    }
-}
+       }}}
 console.log(tab);
