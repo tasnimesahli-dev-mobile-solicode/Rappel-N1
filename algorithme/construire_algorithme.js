8d126b07a7b7c7df423ca29dde013aebf5186792
tasnimsahli97@gmail.com <tasnimsahli97@gmail.com>
2026-09-17 16:11:14 +0100

    algorithme

--- algorithme/construire_algorithme.js
+++ algorithme/construire_algorithme.js
@@ -1,32 +1,64 @@
-let videos = [
-    {
-        titre: "A",
-        duree: 3,
-        views: 1500
-    },
-    {
-        titre: "B",
-        duree: 2,
-        views: 2500
-    },
-    {
-        titre: "C",
-        duree: 4,
-        views: 3000
-    },
-    {
-        titre: "D",
-        duree: 1,
-        views: 2100
-    },
-    {
-        titre: "E",
-        duree: 5,
-        views: 1800
-    },
-    {
-        titre: "F",
-        duree: 2,
-        views: 2000
+let produits = [
+    {"produit":"A", "prix":30, "views": 1200},
+    {"produit":"B", "prix":20, "views": 2500},
+    {"produit":"C", "prix":15, "views": 1800},
+    {"produit":"D", "prix":40, "views": 3000},
+    {"produit":"E", "prix":10, "views": 900},
+    {"produit":"F", "prix":25, "views": 2000}
+]
+let populaires = [];
+let taille = 0;
+
+for (let i = 0; i < produits.length; i++) {
+
+    if (produits[i].views > 1500) {
+
+        populaires[taille] = produits[i];
+
+        taille++;
     }
-];
+}
+console.log("Populaire produits: ",populaires);
+
+for (let i = 0; i < populaires.length - 1; i++) {
+
+    for (let j = i + 1; j < populaires.length; j++) {
+
+        if (populaires[i].prix > populaires[j].prix) {
+
+            let temp = populaires[i];
+
+            populaires[i] = populaires[j];
+
+            populaires[j] = temp;
+        }
+    }
+}
+console.log("Trier les produits: ",populaires);
+let budget = 100;
+let total = 0;
+
+let produitsAchetes = [];
+let nombreAchetes = 0;
+
+for (let i = 0; i < populaires.length; i++) {
+
+    if (total + populaires[i].prix <= budget) {
+
+        produitsAchetes[nombreAchetes] = populaires[i];
+
+        nombreAchetes++;
+
+        total = total + populaires[i].prix;
+    }
+}
+
+console.log("Produits achetés :");
+
+for (let i = 0; i < produitsAchetes.length; i++) {
+
+    console.log(produitsAchetes[i].produit);
+}
+
+console.log("Nombre :", nombreAchetes);
+console.log("Total :", total, "DH");
